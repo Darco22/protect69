759e57dafdb7e99432b145cbed6c63ff77ea9d54
Darco22 <57196385+Darco22@users.noreply.github.com>
2021-06-20 14:13:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const bot = new Discord.Client()
 const cfg = require('./index.json');
 const prefix = ("/");
 
-bot.login("NzM0NzczMDI1MTQ4MjM5OTQ0.XxWktA.EK8jg9viAcIZ2lF-drgsAzOi9bk")
+bot.login("Arrete de jouer avec ce compte ! merci")
